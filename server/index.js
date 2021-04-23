37480610a29f1de481a6661ce9f63a21dacd2e17
Mazenode <quentinchaignaud@protonmail.com>
2021-04-23 20:48:45 +0200

    Mode 1 joueur

--- server/index.js
+++ server/index.js
@@ -8,16 +8,14 @@ const io = require("socket.io")(http, {
 
 io.on("connection", (socket) => {
   players.push(`${socket.id.substr(0, 2)}`);
-  console.log(players);
-  if (players.length == 4) {
-    console.log("Il y a déjà 4 joueurs connectés !");
-  }
+  
   console.log("Un nouveau joueur s'est connecté");
 
   socket.on("message", (message) => {
-    console.log(message);
+    console.log(message)
     io.emit("message", `${socket.id.substr(0, 2)} à dit ${message}`);
   });
 });
+
 
 http.listen(8080, () => console.log("Serveur actif sur le port 8080"));
